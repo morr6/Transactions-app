9a2c57abc9a04efc12d69cc06282a6d3c12503f6
marcind <marcin.d@coderspeak.com>
2020-06-07 21:48:48 +0200

    init storage added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { GlobalStyle } from "utilities/GlobalStyles";
+import { initLocalStorage } from "storage/LocalStorage";
 
 import { Homepage } from "homepage/Homepage";
 
 export const App = () => {
+  useEffect(() => {
+    initLocalStorage();
+  }, []);
+
   return (
     <>
       <GlobalStyle />
