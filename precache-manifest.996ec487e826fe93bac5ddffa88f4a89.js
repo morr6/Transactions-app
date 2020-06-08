db43f5ce34f49be43a741764a3c3b510dadf504c
marcind <marcin.d@coderspeak.com>
2020-06-08 03:50:42 +0200

    Updates

---
+++ precache-manifest.996ec487e826fe93bac5ddffa88f4a89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8275ac8bd65a4122c5170eec2fa8acf",
+    "url": "/Transactions-app/index.html"
+  },
+  {
+    "revision": "b31ca16e55faad31143d",
+    "url": "/Transactions-app/static/js/2.dcd023b3.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/Transactions-app/static/js/2.dcd023b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7655a6695caf9461d47d",
+    "url": "/Transactions-app/static/js/main.2f6fa507.chunk.js"
+  },
+  {
+    "revision": "969cdc239b590c72100b",
+    "url": "/Transactions-app/static/js/runtime-main.35245e43.js"
+  }
+]);
